353e53cc840aa3508d5574644b894e5737cf5384
Jonathan Bruckman <jbruckman@gmail.com>
2017-04-10 21:59:03 -0500

    Pushing code.

---
+++ DungeonMasterScreen.Interfaces/Models/CharacterSheet/IAbilities.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonMasterScreen.Interfaces
+{
+    public interface IAbilities
+    {
+        IStrength Strength { get; set; }
+        IDexterity Dexterity { get; set; }
+        IConstitution Constitution { get; set; }
+        IIntelligence Intelligence { get; set; }
+        IWisdom Wisdom { get; set; }
+        ICharisma Charisma { get; set; }
+    }
+}
